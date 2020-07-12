fc5e87d1806b3b98ac630fd6fb4ad70babf266fe
SandyOliveira <sandyoliveira828@gmail.com>
2020-07-12 11:50:43 -0300

    Cadastro de usuario e geração de hash da senha funcionando

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Sandy',
-        email: 'sandy@hand.com.br',
-        password_hash: '12345678',
-    });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
